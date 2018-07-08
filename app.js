63c0ddc3bc6c89d8ad94347f5f28a4cf89812f0d
Rian Perassoli <rian.perassoli@gmail.com>
2018-07-07 23:22:28 -0300

    formatação da tabela

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ var sassMiddleware = require('node-sass-middleware');
 var connectDatabase = require('./models/mongoose').connectDatabase;
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 var recordsRouter = require('./routes/records');
 
 connectDatabase();
@@ -32,7 +31,6 @@ app.use(sassMiddleware({
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/records', recordsRouter);
 
 // catch 404 and forward to error handler
