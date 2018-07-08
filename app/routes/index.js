92bbce3ca98b78cc28c09ebe00848b17c2e0f88b
Rian Perassoli <rian.perassoli@gmail.com>
2018-07-07 21:10:48 -0300

    Operações de crud funcionando

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
+var Record = require('../models/record')
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+  Record.find({}, function(err, records){
+  	if (err) throw new Error(err); 	
+
+  	res.render('index', {records: records});
+  });	  
 });
+
+
+
 
 module.exports = router;
--- app/routes/records.js
+++ app/routes/records.js
@@ -3,23 +3,65 @@ var router = express.Router();
 
 var Record = require('../models/record');
 
-/* GET users listing. */
-router.post('/', function(req, res, next) {
-
-  var data = req.body;
-
-  var record = new Record({
+function fillRecordFromForm(data){
+  return {
     description: data.description,
     value: data.value,
     date: data.date,
     type: data.type 
-  });
+  }
+}
 
-
+function saveAndRedirect(record, res){
   record.save(function (err) {
-    if (err) throw new Error(err);
+
+    if (err) next(err);
   
     console.log('record inserted successfully');
+
+    res.redirect('/');
+
+  });
+}
+
+router.post('/', function(req, res, next) {
+  var data = req.body;
+
+  var id = data.id;
+
+  if (id) {
+    Record.findOne({_id: id}, function(err, record){
+      if(err) next(err);
+
+      record.set(fillRecordFromForm(data));
+
+      saveAndRedirect(record, res)
+    });
+  } else {
+    var record = new Record(fillRecordFromForm(data));
+
+    saveAndRedirect(record, res);
+  }
+});
+
+router.get('/new', function(req, res, next) {
+  res.render('form', {record: null});
+});
+
+
+router.get('/:id/delete', function(req, res, next) {
+  Record.deleteOne({_id: req.params.id}, function(err){
+    if (err) next(err);
+
+    res.redirect('/');
+  });  
+});
+
+router.get('/:id/edit', function(req, res, next) {
+  Record.findOne({_id: req.params.id}, function(err, record){
+    if (err) next(err);
+
+    res.render('form', {record: record});
   });
 });
 
